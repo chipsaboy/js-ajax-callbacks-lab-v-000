b9ef03573529e9a1c11faa0c16a1eaefd6c3fb81
Nathan Campbell <nathancampbell@outlook.com>
2017-10-06 18:48:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 $(document).ready(function (){
 });
 
-var displayError = function() => {
-  $.('#errors').html("An error occurred, please try again.");
-}
+var displayError = () => $('#errors').html("An error occurred, please try again.")
