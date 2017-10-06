ac335a4295ffe92fe55c76900d1c832bbf533588
Nathan Campbell <nathancampbell@outlook.com>
2017-10-06 18:49:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ $(document).ready(function (){
 });
 
 var displayError = () => $('#errors').html("An error occurred, please try again.")
+
+var searchRepositories = () => {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      $('#results').html(renderSearchResults(data))
+    }).fail(error => {
+      displayError()
+    })
+}
